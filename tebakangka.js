4ee1c4b14919365b81947c1bf77e8427c0c8350c
erwinnurhaq <simkalastoforka@gmail.com>
2019-04-26 00:18:53 +0700

    Coba tebak-tebakan angka

---
+++ tebakangka.js
@@ -0,0 +1,21 @@
+var x,
+    a,
+    ht;
+
+x = Math.floor((Math.random() * 10) + 1);
+
+
+function cek() {
+    a = document.getElementById("inputAngka").value;
+    if (a > x) {
+        ht = "Angka kelebihan, kurangi lagi!";
+    } else if (a < x) {
+        ht = "Angka kekecilan, tambah lagi!";
+    } else {
+        ht = "Anda Benar!";
+        document.getElementById("hasil").innerHTML = x;
+        document.getElementById("lagitext").innerHTML = "reload";
+    }
+
+    document.getElementById("hasiltext").innerHTML = ht;
+}
